da0c492a727f9514e3a002227b6a8b6ca6be9a0a
Elizaveta Oleynik <oleynik.elizaveta1995@gmail.com>
2021-02-11 08:43:55 +0300

    ...

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
-const tabsBtn = document.querySelectorAll('.tabs__nav-btn')
-const tabsItems = document.querySelectorAll('.tabs__item')
+const tabsBtn = document.querySelectorAll('.tabs__nav-btn') // выбрали все кнопки
+const tabsItems = document.querySelectorAll('.tabs__item') // выбрали все контенты табов
 
-tabsBtn.forEach(onTabClick)
+tabsBtn.forEach(onTabClick) //метод forEach
+// Метод «arr.forEach(callback[, thisArg])» используется для перебора массива. Он для каждого элемента массива вызывает функцию callback. Этой функции он передаёт три параметра callback(item, i, arr):
 
-function onTabClick(item){
-    item.addEventListener('click', function(){
-        let currentBtn = item
-        let tabId = currentBtn.getAttribute('data-tab')
-        let currentTab = document.querySelector(tabId)
+function onTabClick(item){ // item - каждая конкретная кнопка
+    item.addEventListener('click', function(){ // обработчик событий click
+        
+        let currentBtn = item // текущая кнопка
+        let tabId = currentBtn.getAttribute('data-tab') // значение атрибута заключили в переменную, Получить атрибут 
+        let currentTab = document.querySelector(tabId) // таб, который мы хотим показывать, будем выбирать его по id
 
-        if(! currentBtn.classList.contains('active')){
-            tabsBtn.forEach(function(item){
+        if(! currentBtn.classList.contains('active')){ // если кнопка имеет класс active, то...
+            tabsBtn.forEach(function(item){ // ... то для каждой конкретной кнопки 
+                item.classList.remove('active') // ... удалить класс active
+            })
+    
+            tabsItems.forEach(function(item){ // ... то для контента таба удалить класс active
                 item.classList.remove('active')
             })
     
-            tabsItems.forEach(function(item){
-                item.classList.remove('active')
-            })
-    
-            currentBtn.classList.add('active')
-            currentTab.classList.add('active')
+            currentBtn.classList.add('active') // и добавить класс active для кнопки
+            currentTab.classList.add('active') // и добавить класс active для контента таба
         }
     })
 }
 
-document.querySelector('.tabs__nav-btn:nth-child(2)').click();
+document.querySelector('.tabs__nav-btn:nth-child(2)').click(); // имитация нажатия второй кнопки по умолчанию
